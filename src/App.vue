8605c93016d96754c75163e673807bbc07f51d9c
evgeny <afazhenya2001@gmail.com>
2024-10-21 23:43:54 +0300

    tw setup

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
-<template>
-  <div />
-</template>
-
 <script lang="ts">
 
 export default {
-  components: {
-    
-  }
+  components: {}
 }
 </script>
+
+<template>
+  <div />
+</template>
 
 <style scoped>
 
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import PrimeVue from 'primevue/config';
 import Material from '@primevue/themes/material';
 import App from './App.vue'
+import './main.css'
 
 const app = createApp(App)
 app.use(PrimeVue, {
